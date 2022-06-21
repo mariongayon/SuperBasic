44c355d4f567dd4dfc177f212af571130f66e771
Marion Gayon <mariongayon@gmail.com>
2022-06-21 12:10:43 +0200

    add function SIN

---
+++ Functions/Sin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    public class Sin : IFunction
+    {
+        float IFunction.Apply(List<BasicNumber> arguments)
+        {
+            return (float)Math.Sin(arguments[0]);
+        }
+    }
+}
+
+    
