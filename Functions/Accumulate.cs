c0babe449505cfb6c075ab456b5bb2c0d6de1bdf
Marion Gayon <mariongayon@gmail.com>
2022-06-21 13:35:05 +0200

    add accumulate function

---
+++ Functions/Accumulate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperBASIC.Functions
+{
+    class Accumulate : IFunction
+    {
+
+        public static float Sum(int start, int end) 
+        {
+            float total = 0;
+
+            for (var i = start; i <= end; i++) 
+            {
+                total += Memory.MemoryGet((short)i);
+            }
+            return total;
+        }
+
+        float IFunction.Apply(List<BasicNumber> arguments)
+        {
+            float a = arguments[0];
+            float b = arguments[1];
+
+            return Sum((int)a, (int)b);
+        }
+    }
+}
